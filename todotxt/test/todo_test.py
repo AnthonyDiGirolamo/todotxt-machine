e561ee1aa2d529d11d70319304773171a9d480e5
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-10-19 12:06:58 -0400

    removed unittest style tests

--- todotxt/test/todo_test.py
+++ todotxt/test/todo_test.py
@@ -1,24 +1,24 @@
-import unittest
+import pytest
 from .. import todo
 
-class TestTodo(unittest.TestCase):
+@pytest.fixture
+def todos():
+  return todo.Todo([
+    "(A) Thank Mom for the meatballs @phone",
+    "(B) Schedule Goodwill pickup +GarageSale @phone",
+    "Post signs around the neighborhood +GarageSale",
+    "@GroceryStore Eskimo pies" ])
 
-  def setUp(self):
-    self.todo_items = [
-      "(A) Thank Mom for the meatballs @phone",
-      "(B) Schedule Goodwill pickup +GarageSale @phone",
-      "Post signs around the neighborhood +GarageSale",
-      "@GroceryStore Eskimo pies" ]
-    self.todos = todo.Todo(self.todo_items)
+def test_todo_init(todos):
+  assert todos.items == [
+    "(A) Thank Mom for the meatballs @phone",
+    "(B) Schedule Goodwill pickup +GarageSale @phone",
+    "Post signs around the neighborhood +GarageSale",
+    "@GroceryStore Eskimo pies" ]
 
-  def test_todo_init(self):
-    assert self.todos.items == self.todo_items
+def test_todo_contexts(todos):
+  assert "@phone" in todos.contexts("(A) Thank Mom for the meatballs @phone")
 
-  def test_todo_contexts(self):
-    assert "@phone" in self.todos.contexts("(A) Thank Mom for the meatballs @phone")
+def test_todo_projects(todos):
+  assert "+GarageSale" in todos.projects("(B) Schedule Goodwill pickup +GarageSale @phone")
 
-  def test_todo_projects(self):
-    assert "+GarageSale" in self.todos.projects("(B) Schedule Goodwill pickup +GarageSale @phone")
-
-if __name__ == '__main__':
-  unittest.main()
