fe38b29795848001386b619e5ba2b0435e16c4c9
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-10-19 13:47:13 -0400

    more tests!

--- todotxt/test/todo_test.py
+++ todotxt/test/todo_test.py
@@ -6,19 +6,33 @@ def todos():
   return todo.Todo([
     "(A) Thank Mom for the meatballs @phone",
     "(B) Schedule Goodwill pickup +GarageSale @phone",
+    "Unpack the guest bedroom +Unpacking",
     "Post signs around the neighborhood +GarageSale",
     "@GroceryStore Eskimo pies" ])
 
 def test_todo_init(todos):
-  assert todos.items == [
-    "(A) Thank Mom for the meatballs @phone",
-    "(B) Schedule Goodwill pickup +GarageSale @phone",
-    "Post signs around the neighborhood +GarageSale",
-    "@GroceryStore Eskimo pies" ]
+  assert len(todos.items) == 5
 
 def test_todo_contexts(todos):
   assert "@phone" in todos.contexts("(A) Thank Mom for the meatballs @phone")
+  assert ["@home", "@phone"] == todos.contexts("Make phonecalls from home @phone @home")
 
 def test_todo_projects(todos):
   assert "+GarageSale" in todos.projects("(B) Schedule Goodwill pickup +GarageSale @phone")
+  assert ["+deck", "+portch"] == todos.projects("Finish outdoor projects +portch +deck")
 
+def test_todo_all_contexts(todos):
+  assert ["@GroceryStore", "@phone"] == todos.all_contexts()
+
+def test_todo_all_projects(todos):
+  assert ["+GarageSale", "+Unpacking"] == todos.all_projects()
+
+def test_todo_creation_date(todos):
+  assert todos.creation_date("2011-03-02 Document +TodoTxt task format") == "2011-03-02"
+  assert todos.creation_date("(A) 2011-03-02 Call Mom") == "2011-03-02"
+  with pytest.raises(todo.NoCreationDateError):
+    assert todos.creation_date("(A) Call Mom 2011-03-02")
+
+def test_todo_due_date(todos):
+  assert todos.due_date("2011-03-02 Document +TodoTxt task format due:2013-10-25") == "2013-10-25"
+  assert todos.due_date("2011-03-02 due:2013-10-25 Document +TodoTxt task format") == "2013-10-25"
--- todotxt/todo.py
+++ todotxt/todo.py
@@ -1,25 +1,60 @@
 #!/usr/bin/env python
 import re
 
+class NoCreationDateError(Exception):
+  pass
+class NoDueDateError(Exception):
+  pass
+
 class Todo:
   """Todo items"""
 
   def __init__(self, todo_items):
     self.items = todo_items
-    self._context_regex = re.compile(r'\s*(@\S+)\s*')
-    self._project_regex = re.compile(r'\s*(\+\S+)\s*')
+    self._context_regex       = re.compile(r'\s*(@\S+)\s*')
+    self._project_regex       = re.compile(r'\s*(\+\S+)\s*')
+    self._creation_date_regex = re.compile(r'^\(?\w?\)?\s*(\d\d\d\d-\d\d-\d\d)\s*')
+    self._due_date_regex      = re.compile(r'\s*due:(\d\d\d\d-\d\d-\d\d)\s*')
 
   def contexts(self, item):
-    return self._context_regex.findall(item)
+    return sorted( self._context_regex.findall(item) )
 
   def projects(self, item):
-    return self._project_regex.findall(item)
+    return sorted( self._project_regex.findall(item) )
 
   def all_contexts(self):
-    all_contexts = []
-    for item in self.items:
-      for found_context in self.contexts(item):
-        if found_context not in all_contexts:
-          all_contexts.append(found_context)
-    return all_contexts
+    # Nested Loop
+    # all_contexts = []
+    # for item in self.items:
+    #   for found_context in self.contexts(item):
+    #     if found_context not in all_contexts:
+    #       all_contexts.append(found_context)
+    # return all_contexts
+
+    # List comprehension
+    # return sorted(set( [found_context for item in self.items for found_context in self.contexts(item)] ))
+
+    # Join all items and use one regex.findall
+    return sorted(set( self._context_regex.findall(" ".join(self.items))))
+
+  def all_projects(self):
+    # List comprehension
+    # return sorted(set( [project for item in self.items for project in self.projects(item)] ))
+
+    # Join all items and use one regex.findall
+    return sorted(set( self._project_regex.findall(" ".join(self.items))))
+
+  def creation_date(self, item):
+    match = self._creation_date_regex.search(item)
+    if match and len(match.groups()) == 1:
+      return match.group(1)
+    else:
+      raise NoCreationDateError
+
+  def due_date(self, item):
+    match = self._due_date_regex.search(item)
+    if match and len(match.groups()) == 1:
+      return match.group(1)
+    else:
+      raise NoDueDateError
 
