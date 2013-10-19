59fa073d3067e21343d62c452e307d286ac58634
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-10-19 14:27:57 -0400

    added a Todo container class
    removed nodate/priority errors, bad idea?

--- todotxt/test/todo_test.py
+++ todotxt/test/todo_test.py
@@ -3,15 +3,45 @@ from .. import todo
 
 @pytest.fixture
 def todos():
-  return todo.Todo([
-    "(A) Thank Mom for the meatballs @phone",
+  return todo.Todos([
+    "(A) Thank Mom for the dinner @phone",
     "(B) Schedule Goodwill pickup +GarageSale @phone",
-    "Unpack the guest bedroom +Unpacking",
-    "Post signs around the neighborhood +GarageSale",
+    "Unpack the guest bedroom +Unpacking due:2013-10-20",
+    "2013-10-19 Post signs around the neighborhood +GarageSale",
     "@GroceryStore Eskimo pies" ])
 
 def test_todo_init(todos):
-  assert len(todos.items) == 5
+  assert len(todos.raw_items)  == 5
+  assert len(todos.todo_items) == 5
+
+  todo = todos.todo_items[0]
+  assert todo.raw      == "(A) Thank Mom for the dinner @phone"
+  assert todo.contexts == ["@phone"]
+  assert todo.projects == []
+  assert todo.priority == "A"
+
+  todo = todos.todo_items[1]
+  assert todo.raw      == "(B) Schedule Goodwill pickup +GarageSale @phone"
+  assert todo.contexts == ["@phone"]
+  assert todo.projects == ["+GarageSale"]
+  assert todo.priority == "B"
+
+  todo = todos.todo_items[2]
+  assert todo.raw      == "Unpack the guest bedroom +Unpacking due:2013-10-20"
+  assert todo.contexts == []
+  assert todo.projects == ["+Unpacking"]
+  assert todo.due_date == "2013-10-20"
+
+  todo = todos.todo_items[3]
+  assert todo.raw           == "2013-10-19 Post signs around the neighborhood +GarageSale"
+  assert todo.contexts      == []
+  assert todo.projects      == ["+GarageSale"]
+  assert todo.creation_date == "2013-10-19"
+
+  todo = todos.todo_items[4]
+  assert todo.raw      == "@GroceryStore Eskimo pies"
+  assert todo.contexts == ["@GroceryStore"]
+  assert todo.projects == []
 
 def test_todo_contexts(todos):
   assert "@phone" in todos.contexts("(A) Thank Mom for the meatballs @phone")
@@ -30,8 +60,8 @@ def test_todo_all_projects(todos):
 def test_todo_creation_date(todos):
   assert todos.creation_date("2011-03-02 Document +TodoTxt task format") == "2011-03-02"
   assert todos.creation_date("(A) 2011-03-02 Call Mom") == "2011-03-02"
-  with pytest.raises(todo.NoCreationDateError):
-    assert todos.creation_date("(A) Call Mom 2011-03-02")
+  # with pytest.raises(todo.NoCreationDateError):
+  assert todos.creation_date("(A) Call Mom 2011-03-02") == ""
 
 def test_todo_due_date(todos):
   assert todos.due_date("2011-03-02 Document +TodoTxt task format due:2013-10-25") == "2013-10-25"
@@ -40,5 +70,5 @@ def test_todo_due_date(todos):
 def test_todo_priority(todos):
   assert todos.priority("(A) Priority A") == "A"
   assert todos.priority("(a) Priority A") == "a"
-  with pytest.raises(todo.NoPriorityError):
-    assert todos.priority("No Priority (A)")
+  # with pytest.raises(todo.NoPriorityError):
+  assert todos.priority("No Priority (A)") == ""
--- todotxt/todo.py
+++ todotxt/todo.py
@@ -1,25 +1,48 @@
 #!/usr/bin/env python
 import re
 
-class NoCreationDateError(Exception):
-  pass
+# class NoCreationDateError(Exception):
+#   pass
 
-class NoDueDateError(Exception):
-  pass
+# class NoDueDateError(Exception):
+#   pass
 
-class NoPriorityError(Exception):
-  pass
+# class NoPriorityError(Exception):
+#   pass
 
 class Todo:
+  """Single Todo item"""
+
+  def __init__(self, item,
+      priority="", contexts=[], projects=[],
+      creation_date="", due_date=""):
+    self.raw           = item
+    self.priority      = priority
+    self.contexts      = contexts
+    self.projects      = projects
+    self.creation_date = creation_date
+    self.due_date      = due_date
+
+
+class Todos:
   """Todo items"""
 
   def __init__(self, todo_items):
-    self.items = todo_items
+    self.raw_items = todo_items
     self._context_regex       = re.compile(r'\s*(@\S+)\s*')
     self._project_regex       = re.compile(r'\s*(\+\S+)\s*')
     self._creation_date_regex = re.compile(r'^\(?\w?\)?\s*(\d\d\d\d-\d\d-\d\d)\s*')
     self._due_date_regex      = re.compile(r'\s*due:(\d\d\d\d-\d\d-\d\d)\s*')
     self._priority_regex      = re.compile(r'\((\w)\)')
+    self.parse_raw_entries()
+
+  def parse_raw_entries(self):
+    self.todo_items = [ Todo(todo,
+      contexts=self.contexts(todo),
+      projects=self.projects(todo),
+      priority=self.priority(todo),
+      creation_date=self.creation_date(todo),
+      due_date=self.due_date(todo)) for todo in self.raw_items]
 
   def contexts(self, item):
     return sorted( self._context_regex.findall(item) )
@@ -30,42 +53,46 @@ class Todo:
   def all_contexts(self):
     # Nested Loop
     # all_contexts = []
-    # for item in self.items:
+    # for item in self.raw_items:
     #   for found_context in self.contexts(item):
     #     if found_context not in all_contexts:
     #       all_contexts.append(found_context)
     # return all_contexts
 
     # List comprehension
-    # return sorted(set( [found_context for item in self.items for found_context in self.contexts(item)] ))
+    # return sorted(set( [found_context for item in self.raw_items for found_context in self.contexts(item)] ))
 
     # Join all items and use one regex.findall
-    return sorted(set( self._context_regex.findall(" ".join(self.items))))
+    return sorted(set( self._context_regex.findall(" ".join(self.raw_items))))
 
   def all_projects(self):
     # List comprehension
-    # return sorted(set( [project for item in self.items for project in self.projects(item)] ))
+    # return sorted(set( [project for item in self.raw_items for project in self.projects(item)] ))
 
     # Join all items and use one regex.findall
-    return sorted(set( self._project_regex.findall(" ".join(self.items))))
+    return sorted(set( self._project_regex.findall(" ".join(self.raw_items))))
 
   def creation_date(self, item):
     match = self._creation_date_regex.search(item)
     if match and len(match.groups()) == 1:
       return match.group(1)
     else:
-      raise NoCreationDateError
+      # raise NoCreationDateError
+      return ""
 
   def due_date(self, item):
     match = self._due_date_regex.search(item)
     if match and len(match.groups()) == 1:
       return match.group(1)
     else:
-      raise NoDueDateError
+      # raise NoDueDateError
+      return ""
 
   def priority(self, item):
     match = self._priority_regex.match(item)
     if match and len(match.groups()) == 1:
       return match.group(1)
     else:
-      raise NoPriorityError
+      # raise NoPriorityError
+      return ""
+
