69b5a7fa8af1e57162bc13106eefe0fce76cd5f8
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-11-05 03:40:20 -0500

    version bump to 1.1.3

--- todotxt_machine/__init__.py
+++ todotxt_machine/__init__.py
@@ -13,7 +13,7 @@ __name__ = "todotxt_machine"
 __title__ = "todotxt_machine"
 __author__ = "Anthony DiGirolamo"
 __license__ = "GPL3"
-__version__ = (1, 1, 2)
+__version__ = (1, 1, 3)
 
 version = "%s.%s.%s" % __version__
 
