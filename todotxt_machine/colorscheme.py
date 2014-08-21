46aceb7152c5721fb4667f66659d5b07cd1d1532
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2014-08-21 09:17:27 -0400

    move colorscheme to its own file

---
+++ todotxt_machine/colorscheme.py
@@ -0,0 +1,51 @@
+import os
+import sys
+
+# Import the correct version of configparser
+if sys.version_info[0] >= 3:
+    import configparser
+    config_parser_module = configparser
+elif sys.version_info[0] < 3:
+    import ConfigParser
+    config_parser_module = ConfigParser
+
+class ColorScheme:
+
+    def __init__(self, name, user_config):
+        self.built_in_colors_directory = os.path.realpath(os.path.expanduser(os.path.dirname(__file__) + '/colors'))
+        self.user_config = user_config
+        self.load_colors(name)
+
+    def load_colors(self, name):
+        self.colors = {}
+        self.focus_map = {}
+        colorscheme_section = "colorscheme-{0}".format(name)
+
+        # Use user defined theme in the user_config if it exists
+        if self.user_config.has_section(colorscheme_section):
+            self.colors = dict( self.user_config.items(colorscheme_section) )
+        else:
+            # Try to load a built in theme
+            cfg = config_parser_module.ConfigParser()
+            if name in os.listdir(self.built_in_colors_directory):
+                cfg.read(self.built_in_colors_directory + "/" + name)
+            # Load default theme
+            else:
+                cfg.read(self.built_in_colors_directory + "/default")
+                colorscheme_section = "colorscheme-default"
+            if cfg.has_section(colorscheme_section):
+                self.colors = dict( cfg.items(colorscheme_section) )
+
+        # Split foreground and background values
+        for key, value in self.colors.items():
+            color_strings = value.split(',')
+            if len(color_strings) == 1:
+                color_strings.append('')
+            self.colors[key] = {'fg': color_strings[0], 'bg': color_strings[1]}
+
+        # Create Selected attributes using the selected_background_color
+        selected_background_color = self.colors['selected']['bg']
+        for key, value in list(self.colors.items()):
+            if key != 'selected':
+                self.colors[key+'_selected'] = {'fg': self.colors[key]['fg'], 'bg': selected_background_color}
+                self.focus_map[key] = key + '_selected'
