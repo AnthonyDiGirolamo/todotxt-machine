f1c77c4b44e397e1508064fb8f5e0226809e2b75
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-08-28 18:01:22 -0400

    make tests pass, add universal wheel option, version bump

--- todotxt_machine/__init__.py
+++ todotxt_machine/__init__.py
@@ -13,7 +13,7 @@ __name__ = "todotxt_machine"
 __title__ = "todotxt_machine"
 __author__ = "Anthony DiGirolamo"
 __license__ = "GPL3"
-__version__ = (2, 1, 0)
+__version__ = (2, 1, 1)
 
 version = "%s.%s.%s" % __version__
 
